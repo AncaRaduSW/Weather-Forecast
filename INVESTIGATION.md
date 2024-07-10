94bdaa134a21118bce28960ada7185c9e09102fd
Anca Radu <anca.radu@softwire.com>
2024-07-10 15:00:13 +0300

    Plots for temperature, precipitation and wind speed

---
+++ INVESTIGATION.md
@@ -0,0 +1,13 @@
+## Investigation File
+
+### Plot for the values of the maximum temperature each day
+![img_4.png](img_4.png)
+
+### Lineplot for the values of the maximum temperature for every year
+![img_1.png](img_1.png)
+
+### Scatterplot for the values of the precipitation for every year
+![img_2.png](img_2.png)
+
+### Scatterplot for the values of the wind speed for every year
+![img_3.png](img_3.png)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 
 def dataset_info(dataframe: pd.DataFrame):
@@ -25,19 +27,40 @@ def dataset_info(dataframe: pd.DataFrame):
 
 
 def temp_max_histplot(dataframe: pd.DataFrame):
-    """ TODO:
-    """
+    sns.histplot(data=dataframe, x='temp_max')
+    plt.ylabel("Count")
+    plt.xlabel("Temperature")
+    plt.title("Temperature Max")
+    plt.show()
 
 
 def temp_max_facegrid_lineplot(dataframe: pd.DataFrame):
-    """ TODO:
-    """
+
+    dataframe['date'] = pd.to_datetime(dataframe['date'])
+    dataframe['month'] = dataframe['date'].dt.month
+    dataframe['year'] = dataframe['date'].dt.year
+
+    g = sns.FacetGrid(dataframe, col="year")
+    g.map_dataframe(sns.lineplot, x="month", y="temp_max")
+    g.set_axis_labels("Month", "Max Temperature(°C)")
+    plt.show()
 
 
 def precipitation_facegrid_scatterplot(dataframe: pd.DataFrame):
     """ TODO:
     """
+    dataframe['date'] = pd.to_datetime(dataframe['date'])
+    dataframe['month'] = dataframe['date'].dt.month
+    dataframe['year'] = dataframe['date'].dt.year
 
+    p = sns.FacetGrid(dataframe, col="year")
+    p.map_dataframe(sns.scatterplot, x="month", y="precipitation")
+    p.set_axis_labels("Month", "Precipitation")
+
+    w = sns.FacetGrid(dataframe, col="year")
+    w.map_dataframe(sns.scatterplot, x="month", y="wind")
+    w.set_axis_labels("Month", "Wind Speed")
+    plt.show()
 
 def weather_countplot(dataframe: pd.DataFrame):
     """ TODO:
@@ -66,7 +89,10 @@ def svr_predictor_default_split(dataframe: pd.DataFrame):
 
 def main():
     df = pd.read_csv('seattle-weather.csv')
-    dataset_info(df)
+    # dataset_info(df)
+    temp_max_histplot(df)
+    # temp_max_facegrid_lineplot(df)
+    # precipitation_facegrid_scatterplot(df)
 
 
 if __name__ == '__main__':
